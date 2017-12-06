19a2c6479e7559c29c324fbe1af90337189b974d
Florian Klampfer <mail@qwtel.com>
2017-12-06 21:14:46 +0100

    remove factory export

--- src/fn.js
+++ src/fn.js
@@ -1,5 +1,4 @@
 import { createCreateElement } from './factory';
 
-export { createCreateElement };
 export const createElement = createCreateElement(document.createElement.bind(document));
 export default createElement;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { createCreateElement } from './factory';
 
-export { createCreateElement };
 export const nativeCreateElement = document.createElement;
 document.createElement = createCreateElement(nativeCreateElement.bind(document));
