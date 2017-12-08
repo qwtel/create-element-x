ec584fb74ac11d5bc3d9a46c17b8b3a0f9faa743
Florian Klampfer <mail@qwtel.com>
2017-12-08 10:20:04 +0100

    read license from fs

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { readFileSync } = require('fs');
 const { resolve } = require('path');
 
 const {
@@ -17,27 +18,9 @@ const { name: filename } = require('./package.json');
 
 const library = camelcase(filename);
 const min = env === 'prod' ? '.min' : '';
-const banner = String.prototype.trim.call(`
-Copyright (c) 2017 Florian Klampfer <https://qwtel.com/>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-`);
+const banner = readFileSync(resolve('LICENSE'), 'utf-8')
+  .replace(/mit license/i, '')
+  .trim();
 
 function envConfig() {
   switch (env) {
