7ac394b3a52372a59b51a97819c51ebe7bd5fd38
Florian Klampfer <mail@qwtel.com>
2017-12-08 10:06:36 +0100

    remove core-js dependency

--- factory.js
+++ factory.js
@@ -1,7 +1,3 @@
-import 'core-js/fn/array/for-each';
-import 'core-js/fn/array/is-array';
-import 'core-js/fn/object/keys';
-
 export const createCreateElement = (createElement, createTextNode) => {
   function appendChild(c) {
     if (typeof c === 'string') this.appendChild(createTextNode(c));
@@ -11,12 +7,11 @@ export const createCreateElement = (createElement, createTextNode) => {
   return (tagName, attributes, children) => {
     const el = createElement(tagName);
 
-    if (attributes) {
-      Object.keys(attributes).forEach(attr => el.setAttribute(attr, attributes[attr]));
-    }
+    for (const attr in attributes) el.setAttribute(attr, attributes[attr]);
 
     if (children) {
-      if (Array.isArray(children)) children.forEach(appendChild, el);
+      if (typeof children === 'string') el.appendChild(createTextNode(children));
+      else if (children.length) Array.prototype.forEach.call(children, appendChild, el);
       else appendChild.call(el, children);
     }
 
