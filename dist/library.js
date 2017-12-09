79b229ea0b3508b52873fd8d3f471879e87c0ab3
Florian Klampfer <mail@qwtel.com>
2017-12-09 15:35:24 +0100

    1.0.0-pre.2

--- dist/library.js
+++ dist/library.js
@@ -111,7 +111,14 @@ var createCreateElement = exports.createCreateElement = function createCreateEle
     for (var attr in attributes) {
       el.setAttribute(attr, attributes[attr]);
     }if (children) {
-      if (typeof children === 'string') el.appendChild(createTextNode(children));else if (children.length) Array.prototype.forEach.call(children, appendChild, el);else el.appendChild(children);
+      if (typeof children === 'string') {
+        el.appendChild(createTextNode(children));
+      } else if (children.length) {
+        var copy = Array.prototype.slice.call(children, 0);
+        Array.prototype.forEach.call(copy, appendChild, el);
+      } else {
+        el.appendChild(children);
+      }
     }
 
     return el;
