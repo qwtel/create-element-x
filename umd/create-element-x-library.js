f238c1a3833ecdab3ce227a1f0713256aad69f59
Florian Klampfer <mail@qwtel.com>
2017-12-10 13:20:15 +0100

    include umd in git for bower

---
+++ umd/create-element-x-library.js
@@ -0,0 +1,133 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("createElementX", [], factory);
+	else if(typeof exports === 'object')
+		exports["createElementX"] = factory();
+	else
+		root["createElementX"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.createElement = undefined;
+
+var _factory = __webpack_require__(1);
+
+var createElement = exports.createElement = (0, _factory.createCreateElement)(document.createElement.bind(document), document.createTextNode.bind(document));
+exports.default = createElement;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var createCreateElement = exports.createCreateElement = function createCreateElement(createElement, createTextNode) {
+  function appendChild(c) {
+    if (typeof c === 'string') this.appendChild(createTextNode(c));else this.appendChild(c);
+  }
+
+  return function (tagName, attributes, children) {
+    var el = createElement(tagName);
+
+    for (var attr in attributes) {
+      el.setAttribute(attr, attributes[attr]);
+    }if (children) {
+      if (typeof children === 'string') {
+        el.appendChild(createTextNode(children));
+      } else if (children.length) {
+        var copy = Array.prototype.slice.call(children, 0);
+        Array.prototype.forEach.call(copy, appendChild, el);
+      } else {
+        el.appendChild(children);
+      }
+    }
+
+    return el;
+  };
+};
+
+exports.default = createCreateElement;
+
+/***/ })
+/******/ ]);
+});
+//# sourceMappingURL=create-element-x-library.js.map
