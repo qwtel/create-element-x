ca899b7ba6b2b11b8136545731003010877913b7
Florian Klampfer <mail@qwtel.com>
2018-06-11 21:32:35 +0700

    Fix #3

--- factory.js
+++ factory.js
@@ -1,23 +1,48 @@
+const isArray = Array.isArray
+  || (arg => Object.prototype.toString.call(arg) === '[object Array]');
+const isHTMLCollection = arg =>
+  Object.prototype.toString.call(arg) === '[object HTMLCollection]';
+const isNodeList = arg =>
+  Object.prototype.toString.call(arg) === '[object NodeList]';
+
 export const createCreateElement = (createElement, createTextNode) => {
   function appendChild(c) {
     if (typeof c === 'string') this.appendChild(createTextNode(c));
     else this.appendChild(c);
   }
 
-  return (tagName, attributes, children) => {
+  return (tagName, attributes, ...children) => {
     const el = createElement(tagName);
 
     for (const attr in attributes) el.setAttribute(attr, attributes[attr]);
 
-    if (children) {
-      if (typeof children === 'string') {
-        el.appendChild(createTextNode(children));
-      } else if (children.length) {
-        const copy = Array.prototype.slice.call(children, 0);
-        Array.prototype.forEach.call(copy, appendChild, el);
-      } else {
-        el.appendChild(children);
+    // Support old JSX sytax that wraps children in an array,
+    // as 3rd parameter.
+    let cs = children[0];
+    let shouldCopy;
+    if (isArray(cs)
+      || (shouldCopy = isHTMLCollection(cs))
+      || (shouldCopy = isNodeList(cs))
+    ) {
+      // In case use user provides a `NodeList` or `HTMLCollection`,
+      // appening will have the effect of removing the item the current
+      // collection, which in turn will cause problems which `forEach`.
+      // So we create a copy frist:
+      if (shouldCopy) {
+        cs = Array.prototype.slice.call(cs, 0);
       }
+      Array.prototype.forEach.call(cs, appendChild, el);
+
+    // Support new JSX syntax, where each child is an additional
+    // function parameter.
+    } else {
+      Array.prototype.forEach.call(children, (child) => {
+        if (typeof child === 'string') {
+          el.appendChild(createTextNode(child));
+        } else {
+          el.appendChild(child);
+        }
+      });
     }
 
     return el;
--- test/index.js
+++ test/index.js
@@ -40,119 +40,231 @@ describe('Create Element Extended', () => {
     assert.equal(div.dataset.foo, 'bar');
   });
 
-  it('should append a child', () => {
-    const child = createElement('span');
-    const div = createElement('div', null, child);
-    assert.equal(div.children.length, 1);
-    assert.equal(div.children[0], child);
-  });
+  describe('children, JSX old', () => {
+    it('should append a child', () => {
+      const child = createElement('span');
+      const div = createElement('div', null, child);
+      assert.equal(div.children.length, 1);
+      assert.equal(div.children[0], child);
+    });
 
-  it('should append multiple children', () => {
-    const div = createElement('div', null, [
-      createElement('span', { id: 'span-1' }),
-      createElement('span', { id: 'span-2' }),
-      createElement('span', { id: 'span-3' }),
-    ]);
-    assert.equal(div.children.length, 3);
-    assert.equal(div.children[0].id, 'span-1');
-    assert.equal(div.children[1].id, 'span-2');
-    assert.equal(div.children[2].id, 'span-3');
-  });
+    it('should append multiple children', () => {
+      const div = createElement('div', null, [
+        createElement('span', { id: 'span-1' }),
+        createElement('span', { id: 'span-2' }),
+        createElement('span', { id: 'span-3' }),
+      ]);
+      assert.equal(div.children.length, 3);
+      assert.equal(div.children[0].id, 'span-1');
+      assert.equal(div.children[1].id, 'span-2');
+      assert.equal(div.children[2].id, 'span-3');
+    });
 
-  it('should nest deep', () => {
-    const div = createElement('div', null,
-      createElement('div', null, [
+    it('should nest deep', () => {
+      const div = createElement('div', null,
         createElement('div', null, [
           createElement('div', null, [
             createElement('div', null, [
               createElement('div', null, [
-                createElement('div'),
+                createElement('div', null, [
+                  createElement('div'),
+                ]),
               ]),
             ]),
           ]),
         ]),
-      ]),
-    );
-    assert(div.children.length);
-    assert(div.children[0].children.length);
-    assert(div.children[0].children[0].children.length);
-    assert(div.children[0].children[0].children[0].children.length);
-    assert(div.children[0].children[0].children[0].children[0].children.length);
-    assert(div.children[0].children[0].children[0].children[0].children[0].children.length);
-  });
+      );
+      assert(div.children.length);
+      assert(div.children[0].children.length);
+      assert(div.children[0].children[0].children.length);
+      assert(div.children[0].children[0].children[0].children.length);
+      assert(div.children[0].children[0].children[0].children[0].children.length);
+      assert(div.children[0].children[0].children[0].children[0].children[0].children.length);
+    });
 
-  it('should append a text node', () => {
-    const span = createElement('div', null, 'Text');
-    assert.equal(span.textContent, 'Text');
-  });
+    it('should append a text node', () => {
+      const span = createElement('div', null, 'Text');
+      assert.equal(span.textContent, 'Text');
+    });
 
-  it('should append multiple text nodes', () => {
-    const span = createElement('div', null, ['Hello', ', ', 'World']);
-    assert.equal(span.textContent, 'Hello, World');
-    assert.equal(span.childNodes.length, 3);
-  });
+    it('should append multiple text nodes', () => {
+      const span = createElement('div', null, ['Hello', ', ', 'World']);
+      assert.equal(span.textContent, 'Hello, World');
+      assert.equal(span.childNodes.length, 3);
+    });
 
-  it('should append text nodes and element nodes', () => {
-    const div = createElement('div', null, [
-      createElement('span', null, 'Hello'),
-      ', ',
-      createElement('span', null, 'World'),
-    ]);
-    assert.equal(div.childNodes.length, 3);
-    assert.equal(div.childNodes[0].textContent, 'Hello');
-    assert.equal(div.childNodes[1].textContent, ', ');
-    assert.equal(div.childNodes[2].textContent, 'World');
-  });
+    it('should append text nodes and element nodes', () => {
+      const div = createElement('div', null, [
+        createElement('span', null, 'Hello'),
+        ', ',
+        createElement('span', null, 'World'),
+      ]);
+      assert.equal(div.childNodes.length, 3);
+      assert.equal(div.childNodes[0].textContent, 'Hello');
+      assert.equal(div.childNodes[1].textContent, ', ');
+      assert.equal(div.childNodes[2].textContent, 'World');
+    });
 
-  describe('NodeList', () => {
-    let div, div2;
-    before(() => {
-      div = createElement('div', null, [
+    describe('NodeList', () => {
+      let div, div2;
+      before(() => {
+        div = createElement('div', null, [
+          createElement('span', { id: 'span-1' }),
+          createElement('span', { id: 'span-2' }),
+          createElement('span', { id: 'span-3' }),
+        ]);
+        div2 = createElement('div', null, div.querySelectorAll('span'));
+      });
+
+      it('should allow passing a NodeList as children', () => {
+        assert.equal(div2.children.length, 3);
+      });
+
+      it('should preserve order', () => {
+        assert.equal(div2.children[0].id, 'span-1');
+        assert.equal(div2.children[1].id, 'span-2');
+        assert.equal(div2.children[2].id, 'span-3');
+      });
+
+      it('should detach nodes form the original node', () => {
+        assert.equal(div.children.length, 0);
+      });
+    });
+
+    describe('HTMLCollection', () => {
+      let div, div2;
+      before(() => {
+        div = createElement('div', null, [
+          createElement('span', { id: 'span-1' }),
+          createElement('span', { id: 'span-2' }),
+          createElement('span', { id: 'span-3' }),
+        ]);
+        div2 = createElement('div', null, div.children);
+      });
+
+      it('should allow passing a HTMLCollection as children', () => {
+        assert.equal(div2.children.length, 3);
+      });
+
+      it('should preserve order', () => {
+        assert.equal(div2.children[0].id, 'span-1');
+        assert.equal(div2.children[1].id, 'span-2');
+        assert.equal(div2.children[2].id, 'span-3');
+      });
+
+      it('should detach nodes form the original node', () => {
+        assert.equal(div.children.length, 0);
+      });
+    });
+  });
+  describe('children, JSX new', () => {
+    it('should append multiple children', () => {
+      const div = createElement('div', null,
         createElement('span', { id: 'span-1' }),
         createElement('span', { id: 'span-2' }),
         createElement('span', { id: 'span-3' }),
-      ]);
-      div2 = createElement('div', null, div.querySelectorAll('span'));
+      );
+      assert.equal(div.children.length, 3);
+      assert.equal(div.children[0].id, 'span-1');
+      assert.equal(div.children[1].id, 'span-2');
+      assert.equal(div.children[2].id, 'span-3');
     });
 
-    it('should allow passing a NodeList as children', () => {
-      assert.equal(div2.children.length, 3);
+    it('should nest deep', () => {
+      const div = createElement('div', null,
+        createElement('div', null,
+          createElement('div', null,
+            createElement('div', null,
+              createElement('div', null,
+                createElement('div', null,
+                  createElement('div'),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+      assert(div.children.length);
+      assert(div.children[0].children.length);
+      assert(div.children[0].children[0].children.length);
+      assert(div.children[0].children[0].children[0].children.length);
+      assert(div.children[0].children[0].children[0].children[0].children.length);
+      assert(div.children[0].children[0].children[0].children[0].children[0].children.length);
     });
 
-    it('should preserve order', () => {
-      assert.equal(div2.children[0].id, 'span-1');
-      assert.equal(div2.children[1].id, 'span-2');
-      assert.equal(div2.children[2].id, 'span-3');
+    it('should append a text node', () => {
+      const span = createElement('div', null, 'Text');
+      assert.equal(span.textContent, 'Text');
     });
 
-    it('should detach nodes form the original node', () => {
-      assert.equal(div.children.length, 0);
-    });
-  });
-
-  describe('HTMLCollection', () => {
-    let div, div2;
-    before(() => {
-      div = createElement('div', null, [
-        createElement('span', { id: 'span-1' }),
-        createElement('span', { id: 'span-2' }),
-        createElement('span', { id: 'span-3' }),
-      ]);
-      div2 = createElement('div', null, div.children);
+    it('should append multiple text nodes', () => {
+      const span = createElement('div', null, 'Hello', ', ', 'World');
+      assert.equal(span.textContent, 'Hello, World');
+      assert.equal(span.childNodes.length, 3);
     });
 
-    it('should allow passing a HTMLCollection as children', () => {
-      assert.equal(div2.children.length, 3);
+    it('should append text nodes and element nodes', () => {
+      const div = createElement('div', null,
+        createElement('span', null, 'Hello'),
+        ', ',
+        createElement('span', null, 'World'),
+      );
+      assert.equal(div.childNodes.length, 3);
+      assert.equal(div.childNodes[0].textContent, 'Hello');
+      assert.equal(div.childNodes[1].textContent, ', ');
+      assert.equal(div.childNodes[2].textContent, 'World');
     });
 
-    it('should preserve order', () => {
-      assert.equal(div2.children[0].id, 'span-1');
-      assert.equal(div2.children[1].id, 'span-2');
-      assert.equal(div2.children[2].id, 'span-3');
+    describe('NodeList', () => {
+      let div, div2;
+      before(() => {
+        div = createElement('div', null,
+          createElement('span', { id: 'span-1' }),
+          createElement('span', { id: 'span-2' }),
+          createElement('span', { id: 'span-3' }),
+        );
+        div2 = createElement('div', null, div.querySelectorAll('span'));
+      });
+
+      it('should allow passing a NodeList as children', () => {
+        assert.equal(div2.children.length, 3);
+      });
+
+      it('should preserve order', () => {
+        assert.equal(div2.children[0].id, 'span-1');
+        assert.equal(div2.children[1].id, 'span-2');
+        assert.equal(div2.children[2].id, 'span-3');
+      });
+
+      it('should detach nodes form the original node', () => {
+        assert.equal(div.children.length, 0);
+      });
     });
 
-    it('should detach nodes form the original node', () => {
-      assert.equal(div.children.length, 0);
+    describe('HTMLCollection', () => {
+      let div, div2;
+      before(() => {
+        div = createElement('div', null,
+          createElement('span', { id: 'span-1' }),
+          createElement('span', { id: 'span-2' }),
+          createElement('span', { id: 'span-3' }),
+        );
+        div2 = createElement('div', null, div.children);
+      });
+
+      it('should allow passing a HTMLCollection as children', () => {
+        assert.equal(div2.children.length, 3);
+      });
+
+      it('should preserve order', () => {
+        assert.equal(div2.children[0].id, 'span-1');
+        assert.equal(div2.children[1].id, 'span-2');
+        assert.equal(div2.children[2].id, 'span-3');
+      });
+
+      it('should detach nodes form the original node', () => {
+        assert.equal(div.children.length, 0);
+      });
     });
   });
 });
